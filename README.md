6134566585d800753961e46629b3d963ae3d8f84
Alexandre Junqueira <alexandre.junqueira@konkerlabs.com>
2020-10-21 17:30:44 -0300

    nova versao

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # ultrasssonando
+
+# references
+
+1. create and deploy a Heroku app 
+   https://medium.com/@blondiebytes/heroku-hosting-put-your-website-on-the-cloud-cdc8729a7e09
+
+2.    
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var port = process.env.PORT || 8080;
 app.use(express.static(__dirname + '/public'));
 
 app.get("/", function(req, res) {
-  res.render('index');
+  res.render('medical-alt');
 });
 
 app.listen(port, function() {
