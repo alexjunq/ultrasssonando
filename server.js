f013cb08e19450facd80afa9473e28a31f1316d5
Alexandre Junqueira <alexandre.junqueira@konkerlabs.com>
2020-12-04 08:32:07 -0300

    favicon

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var favicon = require('serve-favicon');
+var path = require('path');
+
 var app = express();
 
 var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/public'));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 app.get("/", function(req, res) {
   res.render("index");
